5f66a5f0a9fc44ef1b41a79287d22acbcbf1dd54
Pablo <pablo_lopesferrao@hotmail.com>
2022-10-03 17:07:33 -0300

    Ejercicio 8

---
+++ src/tests/OctavoTest.java
@@ -0,0 +1,17 @@
+
+package tests;
+
+import ejercicios.EjercicioOcho;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class OctavoTest {
+
+
+    @Test
+    public void sumaPalindromoHastaElMillon(){// No me estaria saliendo ja, cuando vuelva de la fac sigo probando
+        Assert.assertEquals(EjercicioOcho.main(), 878787);
+    }
+
+
+}
