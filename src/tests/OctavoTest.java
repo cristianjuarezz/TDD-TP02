ed0e955ed9f031e14d9c964bc49d25facbb3ffa3
Cristian Juarez <84170516+cristianjuarezz@users.noreply.github.com>
2022-10-05 16:16:32 -0300

    Aporte de Cesar al Octavo Test

--- src/tests/OctavoTest.java
+++ src/tests/OctavoTest.java
@@ -5,14 +5,23 @@ import ejercicios.EjercicioOcho;
 import org.junit.Assert;
 import org.junit.Test;
 
-/*public class OctavoTest {
-
+public class OctavoTest {
 
     @Test
-    public void sumaPalindromoHastaElMillon(){// No me estaria saliendo ja, cuando vuelva de la fac sigo probando
-        Assert.assertEquals(EjercicioOcho.main(), 878787);
+    public void polindromoEnBinarioYDecimalLaSuma(){
+        Codigo q = new Codigo();
+        
+        long t= q.deDecimalABinario(5);
+        long t1= q.deDecimalABinario(585);
+        
+        assertEquals(101,t);
+        assertEquals(1001001001,t1);
+        
+        int s= q.sumaDePolindromoEnDecimalYBinarioHasta(100000);
+        assertEquals(286577,s);
+        
+        int w= q.sumaDePolindromoEnDecimalYBinarioHasta(1000000);
+        assertEquals(872162,w);
+        
     }
-
-
 }
-*/
